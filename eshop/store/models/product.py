6f2eacda7602c02bb1f297d154fb62fef3adebce
mohiuddinsaumik <1814355021@uits.edu.bd>
2024-03-10 09:31:26 +0600

    Products can be soting by category

--- eshop/store/models/product.py
+++ eshop/store/models/product.py
@@ -4,6 +4,17 @@ from .category import Category
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
-    category = models.ForeignKey(Category, on_delete = models.CASCADE, default = 1)
-    description = models.CharField(max_length=200,default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=200, default='')
     image = models.ImageField(upload_to='uploads/products/')
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
+    @staticmethod
+    def get_all_products_by_category_id(category_id):
+        if category_id:
+            return Product.objects.filter(category__id=category_id)
+        else:
+            return Product.get_all_products()
--- eshop/store/views.py
+++ eshop/store/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from .models.product import Product
+from .models.category import Category
 
 def index(request):
-    return render(request, 'index.html')
+    products = None
+    categories = Category.get_all_categories()
+    category_id = request.GET.get('category')
+    
+    if category_id:
+        products = Product.get_all_products_by_category_id(category_id)
+    else:
+        products = Product.get_all_products()
+    
+    data = {'products': products, 'categories': categories}
+    return render(request, 'index.html', data)
