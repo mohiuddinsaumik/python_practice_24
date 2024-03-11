d974318990f41f55f51dc6b515924d1a83e173f9
mohiuddinsaumik <1814355021@uits.edu.bd>
2024-03-11 22:28:44 +0600

    Sign Up Module done.

---
+++ apna_college-1.py
@@ -0,0 +1,67 @@
+# pylint: disable=invalid-name
+import math
+# age = 33
+#
+# print("My age is {} years old".format(age))
+#
+# print(type(age))
+#
+# a = 2
+# b = 5
+# sum = a+b
+# print(sum)
+#
+
+# a,b = "2",3
+# t = "txt"
+# print(str(a+t)+ str (b))
+
+# a,b = 3,7
+# c = 5
+#
+# print(a+c*b)
+# a = 44
+# b = 2
+# c = a//b
+# print(c)
+#
+# d = a/b
+# print(d)
+#
+# a = math.floor(5.8)
+#
+# print(a)
+"""
+print(-12/5)
+print(12//5)
+"""
+
+# name = input("Name :")
+# print(name)
+
+# a = 5
+# b = 2
+# c = a//b
+# print(c)
+
+# name = input("Enter Name: ")
+# age = int(input("Age: "))
+# read = int(input("Class: "))
+
+# print("My name is {0}. I am {1} year old & I read in class {2}".format(name,age,read))
+
+# x = True
+# y = False
+# r = x and y
+# print(r)
+
+
+# light = int(input("enter number : "))
+
+# if (light ==1):
+#     print("Go")
+# elif(light ==2):
+#     print("Wait")
+# else:
+#     print("Stop")
+
